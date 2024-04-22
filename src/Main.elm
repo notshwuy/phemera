904fe61c396b3f47cfacdc048f163d9a8f1651f5
sxhk0 <nakamidev@gmail.com>
2024-04-22 08:31:53 -0300

    feat: sync tasks in localStorage

--- src/Main.elm
+++ src/Main.elm
@@ -1,4 +1,4 @@
-module Main exposing (..)
+port module Main exposing (..)
 
 import Browser
 
@@ -23,6 +23,9 @@ state = ApplicationState empty_task_list empty_task
 type TaskFormAction = ChangeDescription String | Submit
 type Action = RemoveTask Task | FormAction TaskFormAction
 
+-- PORTS
+port setStorage : (List Task) -> Cmd message
+
 -- Parse, don't validate (https://lexi-lambda.github.io/blog/2019/11/05/parse-don-t-validate/)
 validate_task : Task -> Result () Task
 validate_task task = 
@@ -30,21 +33,18 @@ validate_task task =
     then Ok task
     else Err ()
 
-update : Action -> ApplicationState -> ApplicationState
-update action appState =
-  case action of
+update : Action -> ApplicationState -> (ApplicationState, Cmd message)
+update action appState = case action of
     RemoveTask task -> 
-      { appState | tasks = List.filter (\fromlist -> task /= fromlist) appState.tasks }
+      let stateUpdated = { appState | tasks = List.filter (\fromlist -> task /= fromlist) appState.tasks } in (stateUpdated, setStorage stateUpdated.tasks)
             
     FormAction (ChangeDescription description) ->
-      { appState | draft = { description = description }}
+      let stateUpdated = { appState | draft = { description = description }} in (stateUpdated, Cmd.none)
         
     FormAction Submit -> 
       case validate_task(appState.draft) of
-        Ok task -> { appState | tasks = appState.tasks ++ [task], draft = empty_task }
-        Err _ -> appState
-
--- SUBSCRIPTIONS
+        Ok task -> let stateUpdated = { appState | tasks = appState.tasks ++ [task], draft = empty_task } in (stateUpdated, setStorage stateUpdated.tasks)
+        Err _ -> (appState, Cmd.none)
 
 -- VIEW
 render_task : Task -> Html Action
@@ -65,6 +65,11 @@ view appState =
     ]
   ]
 
+
+-- FLAGS
+init : List Task -> (ApplicationState, Cmd message)
+init savedState = (ApplicationState savedState empty_task, Cmd.none)
+
 -- MAIN
-main : Program () (ApplicationState) Action
-main = Browser.sandbox { init = state, update = update, view = view }
+main : Program (List Task) (ApplicationState) Action
+main = Browser.element { init = init, update = update, view = view, subscriptions = \_ -> Sub.none }
